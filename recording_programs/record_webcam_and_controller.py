b773eef5b73343da0d2fc1f6705b00c9acaa4f12
Sujith Christopher <chrisbon95@gmail.com>
2023-02-28 17:42:32 +0530

    changes

--- recording_programs/record_webcam_and_controller.py
+++ recording_programs/record_webcam_and_controller.py
@@ -162,7 +162,7 @@ if __name__ == "__main__":
             _name = input("Enter the name of the recording: ")
         display = True
         _pth = None # this is default do not change, path gets updated by your input
-        _folder_name = "test" # this is the parent folder name where the data will be saved
+        _folder_name = "omni_9dof_feb_28_2023" # this is the parent folder name where the data will be saved
 
     else:
         print("Arguments passed")
