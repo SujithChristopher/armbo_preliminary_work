9b74661bb1a680d0e4b76ff73369bd137ec9c77c
Sujith Christopher <chrisbon95@gmail.com>
2023-03-06 16:33:41 +0530

    changes

--- support/imu_calculations.py
+++ support/imu_calculations.py
@@ -61,10 +61,6 @@ def calculate_imu_velocity(df, filter_size=51, filter_order=1, zero_drift=50):
     
     _imu_df = df.copy()
 
-    _imu_df["ax"] = savgol_filter(_imu_df["ax"], filter_size, filter_order)
-    _imu_df["ay"] = savgol_filter(_imu_df["ay"], filter_size, filter_order)
-    _imu_df["az"] = savgol_filter(_imu_df["az"], filter_size, filter_order)
-
     val_x = _imu_df["ax"][:zero_drift].mean()
     val_y = _imu_df["ay"][:zero_drift].mean()
     val_z = _imu_df["az"][:zero_drift].mean()
@@ -85,9 +81,48 @@ def calculate_imu_velocity(df, filter_size=51, filter_order=1, zero_drift=50):
             my_dict["vel_y"].append(my_dict["vel_y"][i-1] + (_imu_df["ay"][i] - val_y) * 0.01)
             my_dict["vel_z"].append(my_dict["vel_z"][i-1] + (_imu_df["az"][i] - val_z) * 0.01)
 
-    _accel_df = df.copy()
-    _accel_df["vel_x"] = my_dict["vel_x"]
-    _accel_df["vel_y"] = my_dict["vel_y"]
-    _accel_df["vel_z"] = my_dict["vel_z"]
+    _vel_df = df.copy()
+    _vel_df["vel_x"] = my_dict["vel_x"]
+    _vel_df["vel_y"] = my_dict["vel_y"]
+    _vel_df["vel_z"] = my_dict["vel_z"]
 
-    return _accel_df
+    return _vel_df
+
+
+def calculate_imu_displacement(df, filter_size=51, filter_order=1, zero_drift=0):
+    """
+    df: the dataframe containing the imu data
+    filter_size: the size of the filter window (should be odd)
+    The funciton uses the savgol filter to smooth the data
+    filter_order: the order of the filter
+    zero_drift: the number of rows to use to calculate the zero drift
+
+    the df should contain ax, ay, az columns
+
+    returns: the dataframe with the calculated displacemnt
+    """    
+    
+    _imu_df = df.copy()
+
+    # calculating angle from ax, ay, az
+    _imu_df = pl.from_pandas(_imu_df)
+
+    my_dict = {"disp_x":[],"disp_y":[],"disp_z":[]}
+
+    for i in range(len(_imu_df)):
+        if i == 0:
+            my_dict["disp_x"].append(0)
+            my_dict["disp_y"].append(0)
+            my_dict["disp_z"].append(0)
+        else:
+
+            my_dict["disp_x"].append(my_dict["disp_x"][i-1] + (_imu_df["vel_x"][i]) * 0.01)
+            my_dict["disp_y"].append(my_dict["disp_y"][i-1] + (_imu_df["vel_y"][i]) * 0.01)
+            my_dict["disp_z"].append(my_dict["disp_z"][i-1] + (_imu_df["vel_z"][i]) * 0.01)
+
+    _disp_df = df.copy()
+    _disp_df["disp_x"] = my_dict["disp_x"]
+    _disp_df["disp_y"] = my_dict["disp_y"]
+    _disp_df["disp_z"] = my_dict["disp_z"]
+
+    return _disp_df
