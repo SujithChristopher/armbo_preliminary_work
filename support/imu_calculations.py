1a586307ca37ab4b4dd45a02d7e2a395161c7ee4
Sujith Christopher <chrisbon95@gmail.com>
2023-05-02 16:53:21 +0530

    rewriting some code, not fixed yet

--- support/imu_calculations.py
+++ support/imu_calculations.py
@@ -2,7 +2,7 @@
 from scipy.signal import savgol_filter
 import polars as pl
 
-def calculate_imu_orientation(df, filter_size=51, filter_order=1, zero_drift=50):
+def calculate_imu_orientation(df, filter_size=51, filter_order=1, zero_drift=50, dt = 0.01):
     """
     df: the dataframe containing the imu data
     filter_size: the size of the filter window (should be odd)
@@ -15,38 +15,49 @@ def calculate_imu_orientation(df, filter_size=51, filter_order=1, zero_drift=50)
     returns: the dataframe with the calculated angles
     """    
     
-    _imu_df = df.copy()
+    inst = True
 
-    _imu_df["gy"] = savgol_filter(_imu_df["gy"], filter_size, filter_order)
-    _imu_df["gx"] = savgol_filter(_imu_df["gx"], filter_size, filter_order)
-    _imu_df["gz"] = savgol_filter(_imu_df["gz"], filter_size, filter_order)
+    if not isinstance(df, pl.DataFrame):
+        inst = False 
+        _imu_df = df.copy()
+        df = pl.from_pandas(df)
+
+    else:
+        _imu_df = df
 
     val_x = _imu_df["gx"][:zero_drift].mean()
     val_y = _imu_df["gy"][:zero_drift].mean()
     val_z = _imu_df["gz"][:zero_drift].mean()
 
-    # calculating angle from gx, gy, gz
+    # subtracting the zero drift
+    _imu_df = _imu_df.with_columns([
+                                pl.col("gx") - val_x, 
+                                pl.col("gy") - val_y,
+                                pl.col("gz") - val_z
+                            ])
+    
+    # smoothing the data using savgol filter
+    if filter_order and filter_size is not None:
+        _imu_df = _imu_df.with_columns([
+                                pl.col("gx").map(lambda x: savgol_filter(x.to_numpy(), filter_size, filter_order)).explode(),
+                                pl.col("gy").map(lambda x: savgol_filter(x.to_numpy(), filter_size, filter_order)).explode(),
+                                pl.col("gz").map(lambda x: savgol_filter(x.to_numpy(), filter_size, filter_order)).explode()
+                            ])
+        _imu_df = _imu_df.drop_nulls()
 
-    my_dict = {"angle_gx":[],"angle_gy":[],"angle_gz":[]}
+    # calculating linear velocity from ax, ay, az
+    _imu_df = _imu_df.with_columns([
+                                (pl.col("gx").cumsum()*dt).alias("ang_x"),
+                                (pl.col("gy").cumsum()*dt).alias("ang_y"),
+                                (pl.col("gz").cumsum()*dt).alias("ang_z")
+                            ])
 
-    for i in range(len(_imu_df)):
-        if i == 0:
-            my_dict["angle_gx"].append(0)
-            my_dict["angle_gy"].append(0)
-            my_dict["angle_gz"].append(0)
-        else:
-            my_dict["angle_gx"].append(my_dict["angle_gx"][i-1] + (_imu_df["gx"][i] - val_x) * 0.01)
-            my_dict["angle_gy"].append(my_dict["angle_gy"][i-1] + (_imu_df["gy"][i] - val_y) * 0.01)
-            my_dict["angle_gz"].append(my_dict["angle_gz"][i-1] + (_imu_df["gz"][i] - val_z) * 0.01)
+    if not inst:
+        return _imu_df.to_pandas()
 
-    _gyro_df = df.copy()
-    _gyro_df["angle_gx"] = my_dict["angle_gx"]
-    _gyro_df["angle_gy"] = my_dict["angle_gy"]
-    _gyro_df["angle_gz"] = my_dict["angle_gz"]
+    return _imu_df
 
-    return _gyro_df
-
-def calculate_imu_velocity(df, filter_size=51, filter_order=1, zero_drift=50):
+def calculate_imu_velocity(df, filter_size=None, filter_order=None, zero_drift=50, dt = 0.01):
     """
     df: the dataframe containing the imu data
     filter_size: the size of the filter window (should be odd)
@@ -59,37 +70,51 @@ def calculate_imu_velocity(df, filter_size=51, filter_order=1, zero_drift=50):
     returns: the dataframe with the calculated angles
     """    
     
-    _imu_df = df.copy()
+    inst = True
+
+    if not isinstance(df, pl.DataFrame):
+        inst = False 
+        _imu_df = df.copy()
+        df = pl.from_pandas(df)
+
+    else:
+        _imu_df = df
 
     val_x = _imu_df["ax"][:zero_drift].mean()
     val_y = _imu_df["ay"][:zero_drift].mean()
     val_z = _imu_df["az"][:zero_drift].mean()
 
-    # calculating angle from ax, ay, az
-    _imu_df = pl.from_pandas(_imu_df)
 
-    my_dict = {"vel_x":[],"vel_y":[],"vel_z":[]}
+    # subtracting the zero drift
+    _imu_df = _imu_df.with_columns([
+                                pl.col("ax") - val_x, 
+                                pl.col("ay") - val_y,
+                                pl.col("az") - val_z
+                            ])
+    
+    # smoothing the data using savgol filter
+    if filter_order and filter_size is not None:
+        _imu_df = _imu_df.with_columns([
+                                pl.col("ax").map(lambda x: savgol_filter(x.to_numpy(), filter_size, filter_order)).explode(),
+                                pl.col("ay").map(lambda x: savgol_filter(x.to_numpy(), filter_size, filter_order)).explode(),
+                                pl.col("az").map(lambda x: savgol_filter(x.to_numpy(), filter_size, filter_order)).explode()
+                            ])
+        _imu_df = _imu_df.drop_nulls()
 
-    for i in range(len(_imu_df)):
-        if i == 0:
-            my_dict["vel_x"].append(0)
-            my_dict["vel_y"].append(0)
-            my_dict["vel_z"].append(0)
-        else:
+    # calculating linear velocity from ax, ay, az
+    _imu_df = _imu_df.with_columns([
+                                (pl.col("ax").cumsum()*dt).alias("vx"),
+                                (pl.col("ay").cumsum()*dt).alias("vy"),
+                                (pl.col("az").cumsum()*dt).alias("vz")
+                            ])
 
-            my_dict["vel_x"].append(my_dict["vel_x"][i-1] + (_imu_df["ax"][i] - val_x) * 0.01)
-            my_dict["vel_y"].append(my_dict["vel_y"][i-1] + (_imu_df["ay"][i] - val_y) * 0.01)
-            my_dict["vel_z"].append(my_dict["vel_z"][i-1] + (_imu_df["az"][i] - val_z) * 0.01)
+    if not inst:
+        return _imu_df.to_pandas()
 
-    _vel_df = df.copy()
-    _vel_df["vel_x"] = my_dict["vel_x"]
-    _vel_df["vel_y"] = my_dict["vel_y"]
-    _vel_df["vel_z"] = my_dict["vel_z"]
-
-    return _vel_df
+    return _imu_df
 
 
-def calculate_imu_displacement(df, filter_size=51, filter_order=1, zero_drift=0):
+def calculate_imu_displacement(df, filter_size=None, filter_order=None, zero_drift=0, dt = 0.01):
     """
     df: the dataframe containing the imu data
     filter_size: the size of the filter window (should be odd)
@@ -102,11 +127,23 @@ def calculate_imu_displacement(df, filter_size=51, filter_order=1, zero_drift=0)
     returns: the dataframe with the calculated displacemnt
     """    
     
-    _imu_df = df.copy()
+    inst = True
+
+    if not isinstance(df, pl.DataFrame):
+        inst = False 
+        _imu_df = df.copy()
+        df = pl.from_pandas(df)
+    else:
+        _imu_df = df
+
+    if filter_order and filter_size is not None:
+        _imu_df = _imu_df.with_columns([
+                                pl.col("ax").map(lambda x: savgol_filter(x, filter_size, filter_order)),
+                                pl.col("ay").map(lambda x: savgol_filter(x, filter_size, filter_order)),
+                                pl.col("az").map(lambda x: savgol_filter(x, filter_size, filter_order))
+                            ])
 
     # calculating angle from ax, ay, az
-    _imu_df = pl.from_pandas(_imu_df)
-
     my_dict = {"disp_x":[],"disp_y":[],"disp_z":[]}
 
     for i in range(len(_imu_df)):
@@ -116,11 +153,12 @@ def calculate_imu_displacement(df, filter_size=51, filter_order=1, zero_drift=0)
             my_dict["disp_z"].append(0)
         else:
 
-            my_dict["disp_x"].append(my_dict["disp_x"][i-1] + (_imu_df["vel_x"][i]) * 0.01)
-            my_dict["disp_y"].append(my_dict["disp_y"][i-1] + (_imu_df["vel_y"][i]) * 0.01)
-            my_dict["disp_z"].append(my_dict["disp_z"][i-1] + (_imu_df["vel_z"][i]) * 0.01)
+            my_dict["disp_x"].append(my_dict["disp_x"][i-1] + (_imu_df["vel_x"][i]) * dt)
+            my_dict["disp_y"].append(my_dict["disp_y"][i-1] + (_imu_df["vel_y"][i]) * dt)
+            my_dict["disp_z"].append(my_dict["disp_z"][i-1] + (_imu_df["vel_z"][i]) * dt)
 
-    _disp_df = df.copy()
+    df = df.to_pandas()
+    _disp_df = df
     _disp_df["disp_x"] = my_dict["disp_x"]
     _disp_df["disp_y"] = my_dict["disp_y"]
     _disp_df["disp_z"] = my_dict["disp_z"]
